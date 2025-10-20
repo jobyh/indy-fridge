2d327d77bb0d488c182a5110806df490f1dc1856
Joby Harding <code@fullstackappco.com>
2025-10-20 20:43:54 +0100

    Fix QR scaling

--- resources/views/qr.blade.php
+++ resources/views/qr.blade.php
@@ -14,7 +14,7 @@
 
 <x-layout.html>
 
-    <div class="relative w-sm h-sm border-black">
+    <div class="relative border-black">
         <img
             alt="QR Code link to {{ $url }}"
             src="{{ new QRCode($options)->render($url) }}"
@@ -23,8 +23,7 @@
         <x-icon.crow @class([
             'text-black absolute top-1/2 left-1/2',
             '-translate-x-1/2 -translate-y-[calc(50%+0.5rem)]',
-            'w-28 h-28'
+            'w-[30%] h-[30%]'
         ]) />
-        <p class="absolute font-mono text-xs text-black top-2 left-8">{{ $url }}</p>
     </div>
 </x-layout.html>
