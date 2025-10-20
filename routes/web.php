f8269298ea0f87f4ce2d94d390b901cdc985692e
Joby Harding <code@fullstackappco.com>
2025-10-20 20:31:18 +0100

    Ensure all pages are exported

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,6 @@ Route::view('/', 'search')->name('search');
 
 Route::view('/qr', 'qr')->name('qr');
 
-Route::get('/lydia', function () {
+Route::get('/menu', function () {
     return Pdf::loadView('list')->stream('take-out-cans.pdf');
 })->name('list.pdf');
