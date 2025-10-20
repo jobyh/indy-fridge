66baec73d585a092c83d83ad608fbbcb11b1982c
Joby Harding <code@fullstackappco.com>
2025-10-20 21:46:40 +0100

    Fix tablet layout

--- resources/views/components/layout/header.blade.php
+++ resources/views/components/layout/header.blade.php
@@ -2,27 +2,28 @@
     'bg-gray-900 py-3 border-default border-b',
 ]) }}>
     <div class="w-layout mx-auto px-layout">
-
-        <div class="text-xs flex justify-between items-center -translate-y-1">
-            <div class="flex items-center gap-1 text-nowrap">
-                <x-type tag="h1" class="uppercase tracking-wider flex items-center gap-1.5">
-                    <x-icon.crow class="w-4 h-4 -translate-y-0.5" />
-                    {{ config('app.name') }}
-                </x-type>
-                <x-type tag="p" variant="dim" class="text-[10px] sm:text-xs">
-                    <span class="hidden sm:inline">for the locals</span>
-                    by <x-type.link href="https://fullstackappco.com">Joby</x-type.link>
-                </x-type>
+        <div class="mx-layout md:mx-0">
+            <div class="text-xs flex justify-between items-center -translate-y-1">
+                <div class="flex items-center gap-1 text-nowrap">
+                    <x-type tag="h1" class="uppercase tracking-wider flex items-center gap-1.5">
+                        <x-icon.crow class="w-4 h-4 -translate-y-0.5" />
+                        {{ config('app.name') }}
+                    </x-type>
+                    <x-type tag="p" variant="dim" class="text-[10px] sm:text-xs">
+                        <span class="hidden sm:inline">for the locals</span>
+                        by <x-type.link href="https://fullstackappco.com">Joby</x-type.link>
+                    </x-type>
+                </div>
+                <x-type tag="small" variant="dim" class="text-[10px] sm:text-xs text-nowrap">Search <span class="hidden sm:inline">powered</span> by <x-type.link href="https://algolia.com">Algolia</x-type.link></x-type>
             </div>
-            <x-type tag="small" variant="dim" class="text-[10px] sm:text-xs text-nowrap">Search <span class="hidden sm:inline">powered</span> by <x-type.link href="https://algolia.com">Algolia</x-type.link></x-type>
+
+            <div class="flex w-full items-stretch gap-2">
+                <x-search.widget class="flex-grow" />
+                <span class="hidden md:flex items-center ml-6 mr-2 text-sm">Sort by</span>
+
+                <x-search.sort-order class="md:w-[12rem]" />
+            </div>
+
         </div>
-
-        <div class="flex w-full items-stretch gap-2">
-            <x-search.widget class="flex-grow" />
-            <span class="hidden md:flex items-center ml-6 mr-2 text-sm">Sort by</span>
-
-            <x-search.sort-order class="md:w-[12rem]" />
-        </div>
-
     </div>
 </div>
--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -4,10 +4,10 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
     </x-slot:head>
-    <x-layout.header class="sticky top-0 z-10 px-layout " />
-    <main class="w-layout mx-auto">
-        <x-search.info x-cloak x-data class="mx-layout md:!mx-0 px-layout"  />
-        <x-search.hits x-cloak class="mx-layout md:!mx-0" />
+    <x-layout.header class="sticky top-0 z-10" />
+    <main class="w-layout mx-auto px-layout">
+        <x-search.info x-cloak x-data class="mx-layout lg:!mx-0"  />
+        <x-search.hits x-cloak class="-mx-layout md:!mx-0" />
         <x-loader x-data="loader" />
     </main>
 </x-layout.html>
