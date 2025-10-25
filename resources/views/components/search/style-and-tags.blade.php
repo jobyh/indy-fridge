cc13860eb40e276651c7858da977299e0879154a
Joby Harding <code@fullstackappco.com>
2025-10-25 14:43:16 +0100

    Fix style and tags commas

--- resources/views/components/search/style-and-tags.blade.php
+++ resources/views/components/search/style-and-tags.blade.php
@@ -1,11 +1,10 @@
 <div {{ $attributes->class('flex items-center gap-1.5') }}>
-    <x-type x-html="hit.style + (hit.tags.length ? ', ': '')"></x-type>
+    <x-type x-html="hit.style + (hit.tags.length > 0 ? ', ': '')"></x-type>
     <span x-show="hit.tags.length > 0" class="flex items-center gap-1.5 flex-wrap">
         <template x-for="(tag, index) in hit.tags" :key="tag">
             (
             <span>
-                <span x-text="tag"></span>
-                <span x-show="index < hit.hops.length - 1">, </span>
+                <span x-text="tag + ((index < hit.tags.length - 1) ? ', ' : '')"></span>
             </span>
             )
         </template>
