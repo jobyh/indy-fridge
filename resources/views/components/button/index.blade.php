28f33f8930755c0541ac73ddeb1ff95824ca527b
Joby Harding <code@fullstackappco.com>
2025-10-28 13:01:06 +0000

    Don't scale when button inactive

--- resources/views/components/button/index.blade.php
+++ resources/views/components/button/index.blade.php
@@ -13,5 +13,5 @@
     'bg-gray-700' => $variants->doesntContain('primary'),
     'px-5 py-1.5' => $variants->doesntContain('no-padding'),
     'focus:outline-none focus-visible:ring-3 focus-visible:ring-green-500',
-    'disabled:opacity-50 disabled:cursor-not-allowed',
+    'disabled:opacity-50 disabled:cursor-not-allowed disabled:active:scale-100',
 ]) }}>{{ $slot }}</{{ $tag }}>
