bd7ac0f5cfbd2da192918c361104261835245a5e
Joby Harding <code@fullstackappco.com>
2025-11-05 14:37:20 +0000

    Hide site name on smaller devices

--- resources/views/components/layout/header.blade.php
+++ resources/views/components/layout/header.blade.php
@@ -4,17 +4,12 @@
     <div class="w-layout mx-auto px-layout">
         <div class="mx-layout md:mx-0">
             <div class="text-xs flex justify-between items-center -translate-y-1">
-                <div class="flex items-center gap-1 text-nowrap">
+                <div class="items-center gap-1 text-nowrap hidden md:flex">
                     <x-type tag="h1" class="uppercase tracking-wider flex items-center gap-1.5">
                         <x-icon.crow class="w-4 h-4 -translate-y-0.5" />
                         {{ config('app.name') }}
                     </x-type>
-                    <x-type tag="p" variant="dim" class="text-[10px] sm:text-xs">
-                        <span class="hidden sm:inline">for the locals</span>
-                        by <x-type.link href="https://fullstackappco.com">Joby</x-type.link>
-                    </x-type>
                 </div>
-                <x-type tag="small" variant="dim" class="text-[10px] sm:text-xs text-nowrap">Search <span class="hidden sm:inline">powered</span> by <x-type.link href="https://algolia.com">Algolia</x-type.link></x-type>
             </div>
 
             <div class="flex w-full items-stretch gap-2">
